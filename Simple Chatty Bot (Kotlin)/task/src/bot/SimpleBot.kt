009e91ee2eefac15135770b85c0aef62da077829
Filipe Moraes Mendes <10069692+fmmendes@users.noreply.github.com>
2023-03-05 01:01:31 -0300

    First Commit

---
+++ Simple Chatty Bot (Kotlin)/task/src/bot/SimpleBot.kt
@@ -0,0 +1,9 @@
+package bot
+
+const val botName: String = "Chatty"
+const val birthYear: Int = 2023
+
+fun main() {
+    println("Hello! My name is ${botName}.")
+    println("I was created in ${birthYear}.")
+}
